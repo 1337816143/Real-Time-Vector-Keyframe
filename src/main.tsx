2029923688b64702512a265a4915dec340bfd3d7
1337816143 <1337816143@qq.com>
2026-08-15 11:44:14 +0800

    Install custom mask runtime

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { installCustomMaskRuntime } from './engine/customMaskRuntime';
 import './styles.css';
+
+installCustomMaskRuntime();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
