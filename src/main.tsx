76ad8eec66108bb5c4014aba5bf125e612cf371f
1337816143 <1337816143@qq.com>
2026-08-15 11:54:31 +0800

    Install multi-mask scene runtimes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { installCustomMaskRuntime } from './engine/customMaskRuntime';
+import { installSceneGestureRuntime } from './engine/sceneGestureRuntime';
+import { installSceneRuntime } from './engine/sceneRuntime';
 import './styles.css';
 
 installCustomMaskRuntime();
+installSceneGestureRuntime();
+installSceneRuntime();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
