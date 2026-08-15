cac264df92f89fba98301f5d8a92128332d8c083
1337816143 <1337816143@qq.com>
2026-08-15 12:01:50 +0800

    Install selected-mask interaction adapter

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { installCustomMaskRuntime } from './engine/customMaskRuntime';
 import { installSceneGestureRuntime } from './engine/sceneGestureRuntime';
+import { installSceneInteractionRuntime } from './engine/sceneInteractionRuntime';
 import { installSceneRuntime } from './engine/sceneRuntime';
 import './styles.css';
 
 installCustomMaskRuntime();
 installSceneGestureRuntime();
 installSceneRuntime();
+installSceneInteractionRuntime();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
