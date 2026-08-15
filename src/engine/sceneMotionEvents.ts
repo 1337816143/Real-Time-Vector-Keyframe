02cd54cab82e6c4b5ba28d8ffe8b29b4354c8df8
1337816143 <1337816143@qq.com>
2026-08-15 19:14:28 +0800

    Fix scene motion subscription cleanup typing

--- src/engine/sceneMotionEvents.ts
+++ src/engine/sceneMotionEvents.ts
@@ -2,7 +2,9 @@ const listeners = new Set<() => void>();
 
 export function subscribeSceneMotionTrack(listener: () => void) {
   listeners.add(listener);
-  return () => listeners.delete(listener);
+  return () => {
+    listeners.delete(listener);
+  };
 }
 
 export function notifySceneMotionTrackChanged() {
