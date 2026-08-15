c5f950157706743d967edacf1090d4bccc1aded1
1337816143 <1337816143@qq.com>
2026-08-15 12:08:53 +0800

    Add scene motion track change notifications

---
+++ src/engine/sceneMotionEvents.ts
@@ -0,0 +1,10 @@
+const listeners = new Set<() => void>();
+
+export function subscribeSceneMotionTrack(listener: () => void) {
+  listeners.add(listener);
+  return () => listeners.delete(listener);
+}
+
+export function notifySceneMotionTrackChanged() {
+  listeners.forEach((listener) => listener());
+}
