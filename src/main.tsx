ac92e6125b6e4351b4c9a320d2ccb6dc6109ebff
1337816143 <1337816143@qq.com>
2026-08-15 12:05:06 +0800

    Install scene motion runtime

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from './App';
 import { installCustomMaskRuntime } from './engine/customMaskRuntime';
 import { installSceneGestureRuntime } from './engine/sceneGestureRuntime';
 import { installSceneInteractionRuntime } from './engine/sceneInteractionRuntime';
+import { installSceneMotionRuntime } from './engine/sceneMotionRuntime';
 import { installSceneRuntime } from './engine/sceneRuntime';
 import './styles.css';
 
@@ -10,5 +11,6 @@ installCustomMaskRuntime();
 installSceneGestureRuntime();
 installSceneRuntime();
 installSceneInteractionRuntime();
+installSceneMotionRuntime();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
